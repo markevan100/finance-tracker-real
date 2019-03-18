c4c305bd0eedda0c3600438063fc54729966c451
Mark Modak <mmodak@mmodak.local>
2019-03-18 12:04:19 -0600

    Portfolio view of stocks with add and delete.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
 
   def self.new_from_lookup(ticker_sym)
     begin
@@ -6,6 +8,10 @@ class Stock < ApplicationRecord
       new(name: looked_up.company_name, ticker: looked_up.symbol, last_price: looked_up.latest_price)
     rescue Exception => e
       return nil
-    end 
+    end
+  end
+
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
